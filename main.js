2b48f2dc23535f64042fc54d837be9a3da783af3
Bayden Ibrahim <baydenibrahim@gmail.com>
2022-02-15 21:56:12 -0500

    fixed circles 1 and 2

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
+let click = 0;
+function addNumber() {
+  let circle1 = document.getElementById("circle1text");
+  circle1.innerHTML = parseInt(circle1.innerHTML) + 1;
+}
+
+function addBorder() {
+  let circle2 = document.getElementById("circle2");
+  circle2.setAttribute("stroke", "black");
+}
+
+function removeBorder() {
+  let circle2 = document.getElementById("circle2");
+  circle2.setAttribute("stroke", "white");
+}
+
 function disappearAppear() {
-    var element = document.getElementById("circle3");
-    if (element.style.opacity == 0) {
-        element.style.opacity = 1;
-    } else {
-        element.style.opacity = 0;
-    }
+  let element = document.getElementById("circle3");
+  if (element.style.opacity == 0) {
+    element.style.opacity = 1;
+  } else {
+    element.style.opacity = 0;
+  }
 }
